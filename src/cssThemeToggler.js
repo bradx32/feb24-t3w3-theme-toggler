35ccf98b181661870cad20f4a274ae422277da17
Bradley Richmond <bradx32@hotmail.com>
2024-08-31 10:29:32 +1000

    feat: saving and loading theme working

--- src/cssThemeToggler.js
+++ src/cssThemeToggler.js
@@ -21,23 +21,44 @@ let themes = [
     }
 ];
 
-// Read theme name stored in local storage
-// and update CSS variables based on that name
+// Read theme name stored in local storage 
+// and update CSS variables based on that name 
 function getChosenTheme(){
-
+	let foundTheme = localStorage.getItem("theme");
+	console.log(foundTheme);
+	return foundTheme;
 }
 
-// Set theme name to local storage
-// and update CSS variables based on that name
-function setChosenTheme(){
-
+// Set theme name to local storage 
+// and update CSS variables based on that name 
+function setChosenTheme(newThemeName){
+	localStorage.setItem("theme", newThemeName);
 }
+
+if (getChosenTheme() == null){
+	const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+	if (darkThemeMq.matches) {
+	  // Theme set to dark.
+	  setChosenTheme("dark");
+      console.log("No theme found, applied the dark theme")
+	} else {
+	  // Theme set to light.
+	  setChosenTheme("light");
+      console.log("No theme found, applied the light theme")
+	}
+}
+
+
 
 // Loop through properties key in chosen theme object
 // and apply those properties to CSS
 function updateCssVariables(){
 
 }
+
+let themeToggleButton = document.getElementById("themeToggle");
+themeToggleButton.onclick = toggleTheme;
+// themeToggleButton.addEventListener("click", toggleTheme); This is the same as the above line of code but different syntax
 
 
 let rootElement = document.querySelector(":root");
